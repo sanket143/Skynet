184ec0f71d7bc2aa92c5a546f7285a1073e852fc
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-01-18 12:27:18 +0530

    Successfully generated qrcode

--- encoder.rb
+++ encoder.rb
@@ -1,3 +1,7 @@
+require 'rqrcode'
+require 'rmagick'
+include Magick
+
 file_name = "./poster.jpg"
 
 byteArray = File.binread(file_name)
@@ -5,6 +9,28 @@ byteArray = File.binread(file_name)
 unpacked = byteArray.unpack("H*")
 text = unpacked[0]
 
+
+def getImage(text)
+
+  qrcode = RQRCode::QRCode.new(text)
+  png = qrcode.as_png(
+    bit_depth: 1,
+    border_modules: 2,
+    color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+    color: "black",
+    file: nil,
+    fill: 'white',
+    module_px_size: 6,
+    resize_exactly_to: true,
+    resize_gte_to: true,
+    size: 300
+  )
+
+  img = Image.from_blob(png.to_s)
+  img[0].display
+end
+
+getImage("Sanket")
 
 File.open("output.jpg", 'wb' ) do |output|
   output << unpacked.pack("H*")
